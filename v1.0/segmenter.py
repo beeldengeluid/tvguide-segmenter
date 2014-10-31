b9184be6effac573ba5f7df8c235e6e2656ea4d7
Thijs Leegwater <tleegwater@gmail.com>
2014-10-31 13:45:56 +0100

    Various additions

--- v1.0/segmenter.py
+++ v1.0/segmenter.py
@@ -1,5 +1,19 @@
 #!/usr/bin/python
 
+class Config:
+
+   def __init__(self, configFile):
+
+      self.value = {}
+      configFileHandle = open( configFile, "r" )
+      for line in configFileHandle:
+         if "=" in line:
+            key = re.split('=| ', line)[0]
+            val = re.split('=| ', line)[1]
+            self.value[key] = val
+      configFileHandle.close()
+      
+
 class Line:
    'Common base class for all lines'
 
@@ -11,7 +25,8 @@ class Line:
       self.fontSize = 0
 
    def getFontSize(self):
-      fs = 0-9
+      #return average fontsize
+      fs=0
       for w in self.words:
          fs+=int(w.fontSize)
       self.fontSize = fs / len(self.words)
@@ -19,6 +34,7 @@ class Line:
 
 
    def string(self):
+      #return the whole line.
       s=''
       for w in self.words:
          s+=w.text
@@ -43,8 +59,9 @@ class Line:
             return bb
 
    def getNet(self):
+      #try to find net in current line
+
       netList = ['NEDERLAND 1','NEDERLAND 2','NEDERLAND 3','HILVERSUM 1']
-      #return process.extractOne(self.string(), netList)
 
       for nn in netList:
          n = re.search('([\(|\s]'+nn+')|^'+nn+'', self.string(), re.I)
@@ -52,39 +69,32 @@ class Line:
             return nn
 
    def getTime(self):
-      t = re.findall('([0-23]{1,2}[.|:]{1}[0-59]{2})(?!\d)', l.string(), re.I)
+      #try to find valid time in current line
+      if self.getFontSize() is int(config.value['time_fs']) or int(config.value['time_fs']) is 0:
+         t = re.findall('([0-23]{1,2}[.|:]{1}[0-59]{2})(?!\d)', self.string(), re.I)
+         #print t
+         if len(t) is 1:
+            if int(re.split('[.|:|,]', t[0])[0]) < 23 and int(re.split('[.|:|,]', t[0])[1]) < 60:
+               return t
+         elif len(t) is 2:
+            if int(re.split('[.|:|,]', t[0])[0]) < 23 and int(re.split('[.|:|,]', t[0])[1]) < 60 and int(re.split('[.|:|,]', t[1])[0]) < 24 and int(re.split('[.|:|,]', t[1])[1]) < 60:
+               return t
 
-      if len(t) is 1:
-
-         if re.split('[.|:|,]', t[0])[0] < '23' and re.split('[.|:|,]', t[0])[1] < '60':
-            print t
-            return t
-      elif len(t) is 2:
-         if re.split('[.|:|,]', t[0])[0] < 23 and re.split('[.|:|,]', t[0])[1] < 60 and re.split('[.|:|,]', t[1])[0] < 24 and re.split('[.|:|,]', t[1])[1] < 60:
-            return t
-       
-  
-               #   print "whegfjwybrf", t[0].split('.', 1);
-      #elif len(t) is 2:
-      #   return t[0]+' tot en met '+t[1]
 
    def getDate(self):
-    #print self.fontSize
-    if self.fontSize > 10:
-       months = ['jan', 'feb', 'mrt', 'apr', 'mei', 'jun', 'jul', 'aug', 'sep', 'okt', 'oct', 'nov', 'dec']
-       for month in months:
-          mu = month.upper()
-          ml = month.lower()
-          d = re.findall('([0-9]{1,2}[\s]'+mu+'[.])', self.string(), re.I)
-          if len(d) > 0:
-             return d[0]
-       #d = re.findall('([0-9]{1,2}[\s]'+ml+'[.])', self.string(), re.I)
-       #if len(d) > 0:
-       #   return d[0]
-
-      
+      #if self.getFontSize() is int(config.value['date_fs']) or int(config.value['date_fs']) is 0: 
+      months = ['jan', 'feb', 'mrt', 'apr', 'mei', 'jun', 'jul', 'aug', 'sep', 'okt', 'oct', 'nov', 'dec']
+      for month in months:
+         mu = month.upper()
+         ml = month.lower()
+         d = re.findall('([0-9]{1,2}[\s]'+mu+'[.])', self.string(), re.I)
+         if len(d) > 0:
+            return d[0]
+        
 
    def getBounds(self):
+      #return boundaries for this line
+      
       self.l = None
       self.t = None
       self.r = None
@@ -101,7 +111,6 @@ class Line:
          h = int(self.b) - int(self.t)
          x = int(self.l) 
          y = int(self.t) 
-         #print word.text, x, y, w, h
       return x, y, w, h
 
 
@@ -118,7 +127,6 @@ class Word:
       self.r = r
       self.b = b
       
-   #@property
    def getConfidence(self):
       return self.confidence / len(self.text)
 
@@ -183,12 +191,8 @@ import getopt
 #import xml.etree.cElementTree as ET
 from lxml import etree as ET
 from xml.etree.ElementTree import QName
-from pprint import pprint
 import re
-import json
-from fuzzywuzzy import process
-from fuzzywuzzy import fuzz
-import fuzzy
+
 from xml.dom import minidom
 import json
 
@@ -260,12 +264,15 @@ root.set('version', '1.0')
 
 
 
-for fsroot, dirs, filenames in os.walk(inputdir):
+for fsroot, dirs, filenames in os.walk(inputdir):   #loop all xml's (feed the script with one dir=one edition)
    filenames.sort()
    for f in filenames:
       if str(f) != ".xml" :
          bn = os.path.basename(inputdir)
+         
+         config = Config("/Users/picturae/Desktop/PROJECTS/OMROEPGIDSEN/configs/"+bn+".txt") #load configfile
          print bn
+         
          ff = str(inputdir + "/" + str(f))
          print ff
          tree = ET.parse(ff)
@@ -282,10 +289,10 @@ for fsroot, dirs, filenames in os.walk(inputdir):
             for line in block.iter(str( QName( namespace, 'line' ) )):
                lineobj = Line(sortIndex(block))
                for formatting in line.iter(str( QName( namespace, 'formatting' ) )):
-                  for c in formatting.iter(str( QName( namespace, 'charParams' ) )):
+                  for c in formatting.iter(str( QName( namespace, 'charParams' ) )):  #loop throug all characters
                      
          
-                     if c.get('wordStart') == '1': 
+                     if c.get('wordStart') == '1':       #make a wordobject and add it to the lineobject
                         if word is not '': lineobj.words.append(Word(word, formatting.get('fs'), confidence, word_l, word_t, word_r, word_b))
                         if c.get('charConfidence') is not None: confidence=int(c.get('charConfidence'))
                         word_l=int(c.get('l'))
@@ -300,27 +307,23 @@ for fsroot, dirs, filenames in os.walk(inputdir):
                         if c.get('r') > word_r: word_r=int(c.get('r'))
                         if c.get('b') > word_b: word_b=int(c.get('b'))
                         word+=c.text
-         
+               
                if word is not '': lineobj.words.append(Word(word, formatting.get('fs'), confidence, word_l, word_t, word_r, word_b))
                word=''
-               #confidence=word_l=word_t=word_r=word_b=None
+               
                lineList.append(lineobj)   
-         
-         #lineList.sort(key=lambda x: x.blockIdx, reverse=False)
          
          programList = []
          
          b=n=t=d=''
          
          for l in lineList: 
-            #l.getFontSize()
-            #l.string()
             if l.getDate() is not None: d=l.getDate() 
 
          for l in lineList: 
-            #print l.string(), l.getBounds()
             if l.getNet() is not None: n=l.getNet()   
             if l.getBroadcaster() is not None: b=l.getBroadcaster()
+
             if l.getTime() > 0:
                
                try:
@@ -331,20 +334,17 @@ for fsroot, dirs, filenames in os.walk(inputdir):
                programobj.src = os.path.basename(ff)
 
                if len(l.getTime()) is 1:
+
                   programobj.startTime = l.getTime()[0]
                elif len(l.getTime()) is 2:
+         
                   programobj.startTime = l.getTime()[0]
                   programobj.stopTime = l.getTime()[1]
                programobj.net = n
                programobj.broadcaster = b
                programobj.date = d
                programobj.description.append(l.words)
-         
-               #if l.l < programobj.l: programobj.l=l.l
-               #if l.l < programobj.l: programobj.l=l.l
-               #if l.l < programobj.l: programobj.l=l.l
-               #if l.l < programobj.l: programobj.l=l.l
-         
+
             else:
                try:
                   programobj.description.append(l.words)
@@ -361,76 +361,4 @@ xml = ET.tostring(root,encoding='utf8', pretty_print=True)
 fp = open(bn+".xml", "w")
 fp.write(xml)
 fp.close()
-         #file.write("<html>\n")
-         #file.write("<head>\n")
-         #file.write("<link rel=\"stylesheet\" href=\"style.css\">\n")
-         #file.write("<script type=\"text/javascript\" src=\"http://code.jquery.com/jquery-1.10.2.js\"></script>\n")        
-         #file.write("</head>\n")
-         #file.write("<body>\n")
-         #file.write("<div class=\"menu\">\n")
-         #file.write("<div class=\"menuBox\" id=\"toggleBox\">\n")
-         #file.write("<span>Toggle Layers</span><br />\n")
-         #file.write("<button id=\"strings\" >Strings</button><br />\n")
-         #file.write("</div>\n")
-         #file.write("</div>\n")
-         #file.write("<div id=\"image\">\n")
-         #file.write("<img src=\"file:///Users/picturae/Desktop/GDS002000056_JPG/"+bn+".jpg\" />\n")
-     
-         
-         
-         
-
-         #for p in programList:
-         #   if len(p.string()) < 2000:
-         #      
-         #      program = ET.SubElement(root, 'program')
-         #      program.set('x', str(p.getBounds()[0]))
-         #      program.set('y', str(p.getBounds()[1]))
-         #      program.set('w', str(p.getBounds()[2]))
-         #      program.set('h', str(p.getBounds()[3]))
-         #
-         #      if p.getBounds()[2] < 1000:
-         #         
-         #         file.write("<div class=\"highlighter\" id=\"highlight-string\" style=\" left: "+str(p.getBounds()[0])+"px; top: "+str(p.getBounds()[1])+"px; width: "+str(p.getBounds()[2])+"px; height: "+str(p.getBounds()[3])+"px; filter: alpha(opacity=50)\" ></div>\n")
-         #         startTime = ET.SubElement(program, 'startTime')
-         #         startTime.text = p.startTime
-         #      
-         #         stopTime = ET.SubElement(program, 'stopTime')
-         #         stopTime.text = p.stopTime
-         #      
-         #         net = ET.SubElement(program, 'net')
-         #         net.text = p.net
-         #      
-         #         broadcaster = ET.SubElement(program, 'broadcaster')
-         #         broadcaster.text = p.broadcaster
-         #      
-         #         description = ET.SubElement(program, 'desc')
-         #         description.text = p.string()
-         #
-         #file.write("<script>\n")
-         #file.write("$(\"button[id*=strings]\").click(function () {\n")
-         #file.write("$(\"div[id*=highlight-string]\").toggle();\n")
-         #file.write("});\n")    
-         #file.write("</script>\n")
-         #file.write("</div>\n")
-         #file.write("</body>\n")
-         #file.write("</html>\n")  
-         #file.close()        
-         #print ET.tostring(root,encoding='unicode', pretty_print=True)
-
-
-
-
-
-
-         
-
-
-
-
-
-
-
-
-
-
+ 
